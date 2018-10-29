faf56abba9baf9257dbdaf25c21a9eaf9adaf43f
Mihajlo Krsmanović <mihajlo.krsmanovic@yahoo.com>
2018-10-29 20:54:47 +0100

    added game start, game table with players in game, limited table access only to the users currently in game

--- app.py
+++ app.py
@@ -1,35 +1,53 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
+from game_manager import GameManager
 
 app = Flask(__name__)
 app.config['SECRET KEY'] = 'chinchirorin'
 socketio = SocketIO(app)
+manager = GameManager()
 
 players = []
 
 def show_players():
-    socketio.emit('clear list')
+    socketio.emit('clearList')
     for player in players:
-        socketio.emit('show players', player, callback=messageReceived)
+        socketio.emit('showPlayers', player)
+
+def abort_game():
+    print('Game aborted')
 
 @app.route('/')
-def sessions():
+def index():
     return render_template('index.html')
 
-def messageReceived(methods=['GET', 'POST']):
-    print('message was received!!!')
-
 @socketio.on('join')
-def player_join(json, methods=['GET', 'POST']):
-    print('received join: ' + str(json))
-    players.append(json)
-    show_players() 
+def player_join(player, methods=['GET', 'POST']):
+    print('received join: ' + str(player))
+    if not manager.started:
+        players.append(player)
+        show_players() 
+        print(players)
+        if len(players) >= 2: socketio.emit('showStartButton')
 
 @socketio.on('leave')
-def player_leave(json, methods=['GET','POST']):
-    print('disconnected %s'%json)
-    players.remove(json)
+def player_leave(player, methods=['GET','POST']):
+    print('disconnected %s'%player)
+    if player in players:
+        players.remove(player)
     show_players()
+    print(players)
+    if len(players) < 2: 
+        socketio.emit('hideStartButton')
+        if manager.started:
+            manager.started = False
+            abort_game()
+
+@socketio.on('start')
+def start_game(player):
+    if player in players:
+        socketio.emit('showTable',players)
+        manager.started = True
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,9 +1,6 @@
 var socket = io.connect('http://' + document.domain + ':' + location.port);
 var player
-socket.on('connect', function() {
-    socket.emit('join', {
-        data: 'User Connected'
-    })
+socket.on('connect', function() {    
     var login = $('#join').on('submit', function(e) {
         e.preventDefault()
         let user_name = $('input.nickname').val()
@@ -16,7 +13,7 @@ socket.on('connect', function() {
             money : 2000
         }
         login.hide()
-        $('#table').show()
+        $('#lobby').show()
     })
 })
 
@@ -24,13 +21,41 @@ window.onbeforeunload = function() {
     console.log('qwfpst')
     socket.emit('leave', player)
 };
-socket.on('show players', function(msg) {
-    console.log(msg)
-    if(typeof msg.user_name !== 'undefined') {        
-        $('ul#players').append('<li><b style="color:#000">'+msg.user_name+'</b> '+msg.money+'</li>')
+
+$('#start').click(function() {
+    socket.emit('start',player)
+})
+
+socket.on('showPlayers', function(player) {
+    console.log(player)
+    if(typeof player.user_name !== 'undefined') {        
+        $('#players').append('<li><b style="color:#000">'+player.user_name+'</b> '+player.money+'</li>')
     }
 })
 
-socket.on('clear list', function(){
-    $('ul#players').empty()
+socket.on('showTable', function(players){
+    console.log(players)
+    console.log(player)
+    for(i = 0; i < players.length; i++){
+        if (players[i].user_name === player.user_name){
+            $('#lobby').hide()
+            $('#table').show()
+            for (i = 0; i < players.length; i++) {
+                name = i % 2 == 0 ? '#left' : '#right'
+                $(name).append('<div class="player-container"><p>'+players[i].user_name+'</p><p>Current money: '+players[i].money+'</p></div>')
+            }
+        }
+    }
+})
+
+socket.on('clearList', function(){
+    $('#players').empty()
+})
+
+socket.on('showStartButton', function(){
+    $('#start').show()
+})
+
+socket.on('hideStartButton', function(){
+    $('#start').hide()
 })
